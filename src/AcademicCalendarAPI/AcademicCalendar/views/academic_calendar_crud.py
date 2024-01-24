63c1ef5304103cb11ef1d9c60ffa661adfecdfd5
Samuel Lima da Silva <samuelima.00@outlook.com>
2024-01-24 11:00:24 -0300

    Adiciona autenticação na rota

--- src/AcademicCalendarAPI/AcademicCalendar/views/academic_calendar_crud.py
+++ src/AcademicCalendarAPI/AcademicCalendar/views/academic_calendar_crud.py
@@ -3,21 +3,25 @@ from AcademicCalendar.serializers import *
 
 from django.http import JsonResponse
 
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 @api_view(['POST'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])
 def createCalendar(request):
     data = request.data
-    data['organization'] = 1
+    data['organization'] = request.user.organization.id
     serializer = CalendarSerializer(data = data)
     if serializer.is_valid():
         calendar = serializer.save()
 
         if(calendar.description is None or len(calendar.description) == 0):
             calendar.description = "Calendário Acadêmico " + str(calendar.id)
-
-        serializer = CalendarSerializer(calendar)
+            calendar.save()
+        serializer = CalendarSerializer(calendar)   
 
         return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
     else:
