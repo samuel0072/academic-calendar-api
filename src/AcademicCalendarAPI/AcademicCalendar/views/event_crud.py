a719e7c63e70df64c673e5d8bb22d0720b7ee545
Samuel Lima da Silva <samuelima.00@outlook.com>
2024-01-27 10:02:23 -0300

    Adiciona listagem de events de um calendário acadêmico

--- src/AcademicCalendarAPI/AcademicCalendar/views/event_crud.py
+++ src/AcademicCalendarAPI/AcademicCalendar/views/event_crud.py
@@ -1,3 +1,4 @@
+import sys
 from AcademicCalendar.models import *
 from AcademicCalendar.serializers import *
 
@@ -7,6 +8,8 @@ from rest_framework.decorators import api_view, authentication_classes, permissi
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
+from django.utils.translation import gettext as _
 
 @api_view(['POST'])
 @authentication_classes([JWTAuthentication])
@@ -20,3 +23,25 @@ def create_event(request):
         return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
     else:
         return JsonResponse(serializer.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+@api_view(['GET'])
+@authentication_classes([JWTAuthentication])
+@permission_classes([IsAuthenticated])    
+def list_event(request, id):
+    #busca exata por calendário acadêmico
+    serializer = None
+    try:
+        parsed_id = int(id)
+
+        if(parsed_id > sys.maxsize):
+            raise Exception(_("The passed id is not valid"))
+        
+        events = Event.objects.filter(organization = request.user.organization, academic_calendar__id=parsed_id).order_by('start_date')
+
+        serializer = EventSerializer(data = events, many=True)
+        serializer.is_valid()
+
+    except Exception as e:
+        return Response({"errors": e.args}, status=status.HTTP_400_BAD_REQUEST, content_type="aplication/json")
+    
+    return Response(serializer.data, status=status.HTTP_200_OK, content_type="aplication/json")
